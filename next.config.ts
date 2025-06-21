9bdb4dc074c3a57beed81d853887e2213fde69e9
mashamipacifique <mshamipaccy04@gmail.com>
2025-06-21 13:10:20 +0200

    initialization of the project

---
+++ next.config.ts
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  sassOptions: {
+    sourceMap: true, // required for resolve-url-loader
+  },
+}
+
+module.exports = nextConfig
---
+++ tailwind.config.ts
@@ -0,0 +1,17 @@
+import type { Config } from "tailwindcss"
+
+import { shadcnPreset } from "./src/lib/shadcn-preset"
+
+const config = {
+    darkMode: ["class"],
+    presets: [shadcnPreset],
+  content: [
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}"
+  ],
+    
+} satisfies Config
+
+export default config
